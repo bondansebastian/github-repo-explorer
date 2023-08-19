271495d02412b5305e6eec2cd14766cfc5994bb5
Bondan Sebastian <bondansebastian@gmail.com>
2023-08-19 23:44:01 +0700

    Fixed repo double click bug

--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Card } from 'react-bootstrap';
 import { components } from "@octokit/openapi-types/types";
 
@@ -7,21 +7,17 @@ type RepoProps = {
 }
 
 export default function Repo({ data }: RepoProps) {
-    const linkRef = useRef<HTMLAnchorElement|null>(null);
-    const handleParentClick = () => {
-        linkRef.current?.click();  
-    };
     return (
-        <Card className='repo' style={{ marginBottom: '10px' }} onClick={handleParentClick} title="Open in new tab">
-            <Card.Body style={{ padding: '7.5px' }}>
-                <a href={data.html_url} target='_blank' rel='noreferrer' className='repo-clickable' ref={linkRef}>
+        <Card className='repo' style={{ marginBottom: '10px' }}>
+            <a href={data.html_url} target='_blank' rel='noreferrer' title="Open in new tab" className='repo-clickable'>
+                <Card.Body style={{ padding: '7.5px' }}>
                     <b>{data.name}</b><br />
                     <p className='text-muted' style={{ fontSize: '12px' }}>{data.description}</p>
                     <div className='text-muted' style={{ position: 'absolute', right: '7.5px', bottom: '5px', fontSize: '12px' }}>
                         {data.stargazers_count} <i className="fa-solid fa-star fa-xs" />
                     </div>
-                </a>
-            </Card.Body>
+                </Card.Body>
+            </a>
         </Card>
     )
 }
